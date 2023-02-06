ecc2dd3cf35ca92383a8668efee368a8cf13280d
Franco <francocorniglione5@gmail.com>
2023-02-06 20:36:48 -0300

    setting db

---
+++ Server/index.js
@@ -0,0 +1,12 @@
+require("dotenv").config();
+const server = require("./src/app.js");
+const { conn } = require("./src/db.js");
+
+const port = process.env.PORT || 3001;
+
+// Syncing all the models at once.
+conn.sync({ force: true }).then(() => {
+  server.listen(port, () => {
+    console.log(`Server listening at port ${port}`);
+  });
+});
