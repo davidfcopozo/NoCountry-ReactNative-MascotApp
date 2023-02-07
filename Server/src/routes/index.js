e5df5c65f0352627f3aff264612ba44acdebe5f8
Franco <francocorniglione5@gmail.com>
2023-02-07 18:59:46 -0300

    feature/getNews

--- Server/src/routes/index.js
+++ Server/src/routes/index.js
@@ -1,8 +1,10 @@
 const { Router } = require("express");
 const usersRoute = require("./users");
+const newsRoute = require("./news");
 
 const router = Router();
 
 router.use("/users", usersRoute);
+router.use("/news", newsRoute);
 
 module.exports = router;
