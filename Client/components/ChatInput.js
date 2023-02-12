f4f04fbcc59dc56f9ff0eb6e8f647e089a7b352e
David Francisco <davidfco.pozo@gmail.com>
2023-02-12 11:31:36 -0400

    Cambios de estilos y dependencias sin usar removidas
    Signed-off-by: David Francisco <davidfco.pozo@gmail.com>

--- Client/components/ChatInput.js
+++ Client/components/ChatInput.js
@@ -1,18 +1,14 @@
-import React, { useState, useEffect, useRef, memo } from "react";
-import { View, Text, StyleSheet, TextInput, Platform, Pressable } from "react-native";
-import { useColorScheme } from "nativewind";
-import { SendIcon } from "./Icons";
-import { useTheme } from "@react-navigation/native";
+import React, { useState } from "react";
+import { View, Text, TextInput, Pressable } from "react-native";
 
-const ChatInput = ({ reply, isLeft, username }) => {
+const ChatInput = ({ reply, isReceived, username }) => {
   const [message, setMessage] = useState("");
-  const { colors } = useTheme();
   return (
     <View className="justify-center bg-black dark:bg-white w-[80%] rounded-bl-full rounded-tl-full border border-gray-500/30">
       {reply ? (
         <View className="px-2 justify-center align-start">
           <Pressable className="absolute right-2 top-1"></Pressable>
-          <Text className="mt-1 font-bold">Response to {isLeft ? username : "Me"}</Text>
+          <Text className="mt-1 font-bold">Response to {isReceived ? username : "Me"}</Text>
           <Text className="mt-1">{reply}</Text>
         </View>
       ) : null}
@@ -20,7 +16,7 @@ const ChatInput = ({ reply, isLeft, username }) => {
         <TextInput
           multiline
           placeholder={"Escribe algo..."}
-          className="bg-transparent w-full h-full py-4 justify-center text-black text-sm  "
+          className="bg-transparent w-full h-full py-3 justify-center text-black text-sm  "
           value={message}
           onChangeText={text => setMessage(text)}
         />
