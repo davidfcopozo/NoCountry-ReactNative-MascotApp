244ccc6f25e18b1bd3ae8e9ee3d25ebab6df44c0
Lautaro <lautiisantillan8@gmail.com>
2023-02-13 13:33:01 -0300

    updated Highlights(git pull changes)

--- Client/components/Highlights.js
+++ Client/components/Highlights.js
@@ -1,20 +1,11 @@
-import { Text, View, Image, Pressable, ScrollView } from "react-native";
+import { Text, View, Image, ScrollView } from "react-native";
 import { Children } from "react";
 import { Ionicons } from "@expo/vector-icons";
 import { Link, useTheme } from "@react-navigation/native";
-import { useDispatch, useSelector } from "react-redux";
-import { useEffect } from "react";
-import { fetchUsers } from "../redux/actions";
+import LoadingGif from "./LoadingGif";
 
-const Highlights = ({ Data }) => {
+const Highlights = ({ data }) => {
   const { colors } = useTheme();
-
-  const dispatch = useDispatch();
-  const usersSort = useSelector(state => state.users.users);
-
-  useEffect(() => {
-    dispatch(fetchUsers());
-  }, []);
 
   return (
     <>
@@ -35,55 +26,57 @@ const Highlights = ({ Data }) => {
                       }}
                     />
 
-                  <View className="p-2">
-                    <Text
-                      numberOfLines={1}
-                      style={{ color: colors.text }}
-                      className="text-base font-semibold -mb-1"
-                    >
-                      {card.service}
-                    </Text>
+                    <View className="p-2">
+                      <Text
+                        numberOfLines={1}
+                        style={{ color: colors.text }}
+                        className="text-base font-semibold -mb-1"
+                      >
+                        {card.service}
+                      </Text>
 
-                    <Text numberOfLines={1} style={{ color: colors.textGray }} className="text-sm">
-                      De {card.location}
-                    </Text>
+                      <Text
+                        numberOfLines={1}
+                        style={{ color: colors.textGray }}
+                        className="text-sm"
+                      >
+                        De {card.city}
+                      </Text>
 
-                    <View className="flex flex-row py-2 justify-left items-left gap-x-2">
-                      {card.user_picture ? (
-                        <Image
-                          style={{
-                            width: 35,
-                            height: 35,
-                            resizeMode: "contain"
-                          }}
-                          className="rounded-full"
-                          source={{
-                            uri: card.user_picture
-                          }}
-                        />
-                      ) : (
-                        <Ionicons name="person-circle-outline" size={32} color={colors.text} />
-                      )}
-                      <View>
-                        <Text style={{ color: colors.text }}>{card.name}</Text>
-                        {card.stars ? (
+                      <View className="flex flex-row py-2 justify-left items-left gap-x-2">
+                        {card.profile_pic ? (
+                          <Image
+                            style={{
+                              width: 35,
+                              height: 35,
+                              resizeMode: "contain"
+                            }}
+                            className="rounded-full"
+                            source={{
+                              uri: card.profile_pic
+                            }}
+                          />
+                        ) : (
+                          <Ionicons name="person-circle-outline" size={32} color={colors.text} />
+                        )}
+                        <View>
+                          <Text style={{ color: colors.text }}>{card.name}</Text>
                           <View className="flex flex-row items-center">
                             {Children.toArray(
-                              Array.from(Array(card.stars)).map(star => (
+                              Array.from(Array(card.rating)).map(star => (
                                 <Ionicons name="star" size={10} color="#ffe100" />
                               ))
                             )}
-                            <Text style={{ color: colors.text }} className="text-xs ml-1">
-                              {card.clients ? "(" + card.clients + ")" : undefined}
-                            </Text>
                           </View>
-                        ) : undefined}
+                        </View>
                       </View>
                     </View>
                   </View>
-                </View>
-              </Link>
-            ))
+                </Link>
+              ))
+            )
+          ) : (
+            <LoadingGif />
           )}
         </View>
       </ScrollView>
