34decc47a378013f6d678f3ecfbc19dbb5375363
David Francisco <davidfco.pozo@gmail.com>
2023-02-15 16:34:21 -0400

    Creador de accion de prueba con su respectivo extraReducer
    Signed-off-by: David Francisco <davidfco.pozo@gmail.com>

--- Client/redux/reducers/users.js
+++ Client/redux/reducers/users.js
@@ -1,11 +1,45 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import { fetchUsers, sortUsersByRating, fetchUserById } from "../actions";
+import {
+  createUserWithEmailAndPassword,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+  signOut,
+  sendPasswordResetEmail,
+  updateEmail,
+  updatePassword,
+  signInWithPopup,
+  GoogleAuthProvider,
+  updateProfile,
+  sendEmailVerification
+} from "firebase/auth";
+import { auth } from "../../firebase";
+import axios from "axios";
 
 const initialState = {
   users: [],
   userDetail: {},
   favouriteUsers: []
 };
+
+export const registerUser = createAsyncThunk("users/registerUser", async formData => {
+  try {
+    const { name, surname, email, password, city } = formData;
+    await createUserWithEmailAndPassword(auth, email, password);
+
+    const userData = {
+      name,
+      surname,
+      city,
+      email,
+      uid: auth.currentUser.uid
+    };
+    const response = await axios.post("/users/add", userData);
+    return response.data;
+  } catch (error) {
+    console.log(error);
+  }
+});
 
 const usersReducer = createSlice({
   name: "users",
@@ -28,6 +62,9 @@ const usersReducer = createSlice({
     });
     builder.addCase(fetchUserById.fulfilled, (state, action) => {
       state.userDetail = action.payload;
+    });
+    builder.addCase(registerUser.fulfilled, (state, action) => {
+      state.users = action.payload;
     });
   }
 });
