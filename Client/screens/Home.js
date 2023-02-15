7adab44b0c70e83e30ac6a69c9b356539f27d4b1
Lautaro <lautiisantillan8@gmail.com>
2023-02-15 19:59:31 -0300

    add Modal with form Service

--- Client/screens/Home.js
+++ Client/screens/Home.js
@@ -1,8 +1,12 @@
-import { Text, View, ScrollView, Image, Pressable } from "react-native";
-import { Link, useTheme } from "@react-navigation/native";
+import { Text, View, ScrollView, Pressable, Modal } from "react-native";
+import { useTheme } from "@react-navigation/native";
+import FormService from "../components/FormService";
+import { useState } from "react";
+import { Ionicons } from "@expo/vector-icons";
 
 const Home = () => {
   const { colors } = useTheme();
+  const [openForm, setOpenForm] = useState(false);
 
   return (
     <>
@@ -21,12 +25,18 @@ const Home = () => {
             </Pressable>
           </View>
           <View className="flex justify-center items-center">
-            <Pressable className="flex items-center justify-center p-3 rounded-lg mt-36 bg-violet-700">
+            <Pressable
+              onPress={() => setOpenForm(!openForm)}
+              className="flex items-center justify-center p-3 rounded-lg mt-36 bg-violet-700"
+            >
               <Text className="flex justify-center font-bold text-xl text-white">
                 Ofrece tus servicios
               </Text>
             </Pressable>
           </View>
+          <Modal animationType="slide" transparent={false} visible={openForm}>
+            <FormService openForm={openForm} setOpenForm={setOpenForm}></FormService>
+          </Modal>
         </View>
       </ScrollView>
     </>
