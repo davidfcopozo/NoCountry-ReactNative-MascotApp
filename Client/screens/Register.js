9617ed1eef232273e0ba9e7cc273ccc15f8ec7e3
David Francisco <davidfco.pozo@gmail.com>
2023-02-15 16:35:55 -0400

    Screen de registro principal
    Signed-off-by: David Francisco <davidfco.pozo@gmail.com>

--- Client/screens/Register.js
+++ Client/screens/Register.js
@@ -13,7 +13,7 @@ import { useState } from "react";
 import { useTheme, useNavigation } from "@react-navigation/native";
 import { useAuth } from "../context/AuthContext";
 import { useDispatch } from "react-redux";
-import { addUser } from "../redux/reducers/users";
+import FormSecondScreen from "../screens/FormSecondScreen";
 
 const Register = () => {
   const { colors } = useTheme();
@@ -21,23 +21,25 @@ const Register = () => {
   const [errors, setErrors] = useState({});
   const [loading, setLoading] = useState(false);
   const [valid, setValid] = useState(false);
-  const [name, setName] = useState("");
-  const [surname, setSurname] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [confirmPassword, setConfirmPassword] = useState("");
+  const [screen, setScreen] = useState(0);
   const { signup } = useAuth();
+  const [formData, setFormData] = useState({
+    name: "",
+    surname: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+    city: ""
+  });
 
-  const navigation = useNavigation();
   const dispatch = useDispatch();
+  const navigation = useNavigation();
 
   async function handleSignup() {
     try {
       setErrors("");
       setLoading(true);
-      await signup(email, password, name, surname);
-
-      await navigation.navigate("Perfil");
+      setScreen(1);
     } catch (error) {
       if (error.code === "auth/email-already-in-use") {
         handleError(
@@ -59,36 +61,26 @@ const Register = () => {
     Keyboard.dismiss();
     setValid(true);
 
-    if (!email) {
+    if (!formData.email) {
       handleError("Por favor, introduzca su correo electrónico", "email");
       setValid(false);
-    } else if (!email.match(/\S+@\S+\.\S+/)) {
+    } else if (!formData.email.match(/\S+@\S+\.\S+/)) {
       handleError("Por favor, introduzca un correo válido", "email");
 
       setValid(false);
     }
 
-    if (!name) {
-      handleError("Por favor, introduzca su nombre", "name");
-      setValid(false);
-    }
-
-    if (!surname) {
-      handleError("Por favor, introduzca sus apellidos", "surname");
-      setValid(false);
-    }
-
-    if (!password) {
+    if (!formData.password) {
       handleError("Por favor, introduzca su contraseña", "password");
       setValid(false);
-    } else if (password.length < 5) {
+    } else if (formData.password.length < 5) {
       handleError("Por favor, introduzca una contraseña con al menos 5 caracteres", "password");
       setValid(false);
     }
-    if (!confirmPassword) {
+    if (!formData.confirmPassword) {
       handleError("Por favor, confirme su contraseña", "confirmPassword");
       setValid(false);
-    } else if (password !== confirmPassword) {
+    } else if (formData.password !== formData.confirmPassword) {
       handleError("Las contraseñas no conincide, intentelo de nuevo", "confirmPassword");
       setValid(false);
     }
@@ -105,7 +97,6 @@ const Register = () => {
   return (
     <KeyboardAvoidingView style={{ flex: 1 }} behavior={"padding"}>
       <ScrollView>
-        <View className="justify-center items-center mb-6"></View>
         <View className="gap-y-2 p-8 w-full ">
           <Image
             style={{
@@ -117,51 +108,46 @@ const Register = () => {
           <Text style={{ color: colors.text }} className="text-2xl mb-4 font-bold text-center">
             Unite a MascotApp
           </Text>
-          <InputField
-            label="Nombres"
-            placeholder="Tus nombres"
-            onChangeText={text => setName(text)}
-            error={errors.name}
-          />
-          <InputField
-            label="Apellidos"
-            placeholder="Tus apellidos"
-            onChangeText={text => setSurname(text)}
-            error={errors.surname}
-          />
-          <InputField
-            label="E-Mail"
-            placeholder="Correo electrónico"
-            onChangeText={text => setEmail(text)}
-            error={errors.email}
-          />
-          <InputField
-            label="Contraseña"
-            placeholder="Tu contraseña"
-            onChangeText={text => setPassword(text)}
-            error={errors.password}
-            password
-          />
-          <InputField
-            label="Confirma tu contraseña"
-            placeholder="Repite tu contraseña"
-            onChangeText={text => setConfirmPassword(text)}
-            error={errors.confirmPassword}
-            password
-          />
 
-          <View className=" bg-violet-700 rounded-lg">
-            <Pressable
-              onPress={handleValidation}
-              /* disabled={loading ? true : false} */
-              /* style={{ backgroundColor: disabled ? "gray" : "rgb(109 40 217)" }} */
-              className=" p-3 rounded-lg"
-            >
-              <Text className="text-white text-center font-bold text-lg">Continuar</Text>
-            </Pressable>
-          </View>
+          {screen === 0 ? (
+            <View>
+              <InputField
+                label="E-Mail"
+                placeholder="Correo electrónico"
+                onChangeText={text => setFormData({ ...formData, email: text })}
+                error={errors.email}
+                value={formData.email}
+              />
+              <InputField
+                label="Contraseña"
+                placeholder="Tu contraseña"
+                /*  onChangeText={text => setPassword(text)} */
+                onChangeText={text => setFormData({ ...formData, password: text })}
+                error={errors.password}
+                password
+                value={formData.password}
+              />
+              <InputField
+                label="Confirma tu contraseña"
+                placeholder="Repite tu contraseña"
+                onChangeText={text => setFormData({ ...formData, confirmPassword: text })}
+                error={errors.confirmPassword}
+                password
+                value={formData.confirmPassword}
+              />
+              <View className=" bg-violet-700 rounded-lg">
+                <Pressable onPress={handleValidation} className=" p-3 rounded-lg">
+                  <Text className="text-white text-center font-bold text-lg">Continuar</Text>
+                </Pressable>
+              </View>
+            </View>
+          ) : (
+            <FormSecondScreen formData={formData} setFormData={setFormData} setScreen={setScreen} />
+          )}
         </View>
       </ScrollView>
+
+      {/* <FormSecondScreen formData={formData} setFormData={setFormData} setScreen={setScreen} /> */}
     </KeyboardAvoidingView>
   );
 };
