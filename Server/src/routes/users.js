7b8efc44210f236c09b266acdf73a7426543cccc
Franco <francocorniglione5@gmail.com>
2023-02-17 18:09:06 -0300

    feature/addingRoutesMiddleware

--- Server/src/routes/users.js
+++ Server/src/routes/users.js
@@ -14,21 +14,23 @@ const {
   getUserJobOffers,
   getSearch
 } = require("../controllers/users");
+const decodeToken = require("../middleware");
 
 const router = Router();
 
-router.get("/", getUsers);
+// El middleware decodeToken se aplica a determinadas rutas (son las que se necesita estar logueado para poder acceder)
+router.get("/", decodeToken, getUsers);
 router.get("/rating", getUsersBestRating);
 router.get("/category", getUsersByCategory);
 router.post("/register", addUser);
 router.get("/filter", getUsersByFilter);
 router.get("/search", getSearch);
-router.get("/jobOffers", getUserJobOffers);
-router.get("/favorites/:id/:page", getUserFavourites);
-router.post("/favorites/:id/:favorite", addUserFavourites);
-router.delete("/favorites/:id/:favorite", deleteFavourite);
-router.patch("/:id", updateUser);
-router.delete("/:id", deleteUser);
-router.get("/:id", getUserById);
+router.get("/jobOffers", decodeToken, getUserJobOffers);
+router.get("/favorites/:id/:page", decodeToken, getUserFavourites);
+router.post("/favorites/:id/:favorite", decodeToken, addUserFavourites);
+router.delete("/favorites/:id/:favorite", decodeToken, deleteFavourite);
+router.patch("/:id", decodeToken, updateUser);
+router.delete("/:id", decodeToken, deleteUser);
+router.get("/:id", decodeToken, getUserById);
 
 module.exports = router;
