75722ddb443b9e297062774563844c4580892d9a
Franco <francocorniglione5@gmail.com>
2023-02-20 16:51:59 -0300

    refactor/prettier_formatting and error codes

--- Server/src/controllers/pets.js
+++ Server/src/controllers/pets.js
@@ -3,33 +3,38 @@ const { isValidNumber } = require("./../validations/index");
 
 const getUserPets = async (req, res) => {
   const { id } = req.body;
+
   try {
     const petList = await Pet.findAll({
       where: {
         user_id: id
       }
     });
+
     return res.status(200).json(petList);
-  } catch (err) {
-    res.status(500).json({
-      message: err.message
+  } catch (error) {
+    return res.status(500).json({
+      errorMessage: error.original ? error.original : error
     });
   }
 };
 
 const getPetInfo = async (req, res) => {
   const { id } = req.params;
-  if (isValidNumber(id)) {
-    res.status(400).json({ message: "id must be a number", id: id });
-  }
+
   try {
+    if (isValidNumber(id)) {
+      res.status(400).json({ errorMessage: "id must be a number", id: id });
+    }
+
     const petInfo = await Pet.findOne({
       where: { id }
     });
+
     return res.status(200).json(petInfo);
-  } catch (err) {
+  } catch (error) {
     return res.status(500).json({
-      error: err.message
+      errorMessage: error.original ? error.original : error
     });
   }
 };
@@ -41,6 +46,7 @@ const addPet = async (req, res) => {
     if (!name || !type_id || !age || !breed || !weight || !user_id) {
       return res.status(400).json({ errorMessage: "Missing required fields", body: req.body });
     }
+
     let newPet = await Pet.create({
       user_id,
       type_id,
@@ -50,9 +56,9 @@ const addPet = async (req, res) => {
       weight
     });
 
-    return res.status(200).json({ pet: newPet.dataValues });
-  } catch (err) {
-    return res.status(500).json({ errorMessage: err.message, body: req.body });
+    return res.status(201).json({ pet: newPet.dataValues });
+  } catch (error) {
+    return res.status(500).json({ errorMessage: error.original ? error.original : error });
   }
 };
 
@@ -76,8 +82,8 @@ const updatePet = async (req, res) => {
     )
       .then(result => res.json(result))
       .catch(err => res.json(err));
-  } catch (err) {
-    return res.status(500).json({ message: err.message });
+  } catch (error) {
+    return res.status(500).json({ errorMessage: error.original ? error.original : error });
   }
 };
 
@@ -90,9 +96,10 @@ const deletePet = async (req, res) => {
         id
       }
     });
+
     return res.status(204).json({ message: "Pet successfully deleted." });
-  } catch (err) {
-    return res.status(500).json({ message: err.message });
+  } catch (error) {
+    return res.status(500).json({ errorMessage: error.original ? error.original : error });
   }
 };
 
