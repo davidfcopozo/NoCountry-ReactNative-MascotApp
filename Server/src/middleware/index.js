b3e9f7009bb671e1d823e6b87411b1786d0b53fc
Franco <francocorniglione5@gmail.com>
2023-02-21 17:41:10 -0300

    refactor/revisiting middleware applied to users-routes

--- Server/src/middleware/index.js
+++ Server/src/middleware/index.js
@@ -3,18 +3,22 @@ const admin = require("../../firebase");
 async function decodeToken(req, res, next) {
   try {
     const token = req.headers.authorization.split(" ")[1];
-    const decodeValue = await admin.auth().verifyIdToken(token);
+    const decodedToken = await admin.auth().verifyIdToken(token);
 
     // Si se verifica el token del currentUser, se lo deja pasar a la ruta a través del next
-    if (decodeValue) {
-      console.log(decodeValue);
+    if (decodedToken) {
       return next();
     }
 
     // Caso contrario, se le envía un mensaje informándole que no tiene acceso
     return res.status(401).json({ errorMessage: "Unauthorized" });
   } catch (error) {
-    return res.status(500).json({ errorMessage: "Internal Server Error" });
+    if (error.errorInfo?.code === "auth/argument-error")
+      return res.status(500).json({
+        errorMessage:
+          "Decoding Firebase ID token failed. Make sure you passed the entire string JWT which represents an ID token."
+      });
+    return res.status(500).json({ errorMessage: error });
   }
 }
 
