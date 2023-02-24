c93c479c10ba075cf435c638aeb8f3bcab53921d
Franco <francocorniglione5@gmail.com>
2023-02-24 12:26:15 -0300

    changing userFav frontend

--- Client/components/UserProfile.js
+++ Client/components/UserProfile.js
@@ -13,7 +13,7 @@ import { Link, useTheme } from "@react-navigation/native";
 import { Children, useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import { actionLogin } from "../redux/reducers/users";
-import { AddFavorite, DelFavorite, fetchFavorites, logOutUser } from "../redux/actions";
+import { addFavourite, deleteFavourite, fetchFavourites, logOutUser } from "../redux/actions";
 import { useDispatch } from "react-redux";
 import VisitorOptions from "./VisitorOptions";
 
@@ -33,13 +33,13 @@ const UserProfile = ({ route }) => {
   };
 
   const addFavorite = () => {
-    dispatch(AddFavorite({ id: currentUser.data.auth.id, fav_id: user.id }));
-    dispatch(fetchFavorites(currentUser.data.auth.id));
+    dispatch(addFavourite({ id: currentUser.data.auth.id, fav_id: user.id }));
+    dispatch(fetchFavourites(currentUser.data.auth.id));
   };
 
   const delFavorite = () => {
-    dispatch(DelFavorite({ id: currentUser.data.auth.id, fav_id: user.id }));
-    dispatch(fetchFavorites(currentUser.data.auth.id));
+    dispatch(deleteFavourite({ id: currentUser.data.auth.id, fav_id: user.id }));
+    dispatch(fetchFavourites(currentUser.data.auth.id));
   };
 
   const verifyFavorite = id => {
