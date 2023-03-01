69195aa3b5e8a84fc4f101b679407f66a9aedabb
David Francisco <davidfco.pozo@gmail.com>
2023-03-01 18:54:03 -0400

    Logica de la pantalla de onboarding agregada
    Signed-off-by: David Francisco <davidfco.pozo@gmail.com>

--- Client/screens/Index.js
+++ Client/screens/Index.js
@@ -1,13 +1,37 @@
-import { Text, View, ScrollView, Image, Pressable } from "react-native";
+import { Text, View, ScrollView } from "react-native";
 import Cards from "../components/Cards";
 import Shortcuts from "../components/Shortcuts";
 import CardsData from "../db/cards.json";
 import Blogs from "../components/Blogs";
 import BlogsData from "../db/blogs.json";
-import { Link, useTheme } from "@react-navigation/native";
+import { useTheme } from "@react-navigation/native";
+import { useEffect, useState } from "react";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { useSelector } from "react-redux";
+import OnboardingScreen from "./OnboardingScreen";
 
 const Index = () => {
   const { colors } = useTheme();
+
+  const [firstAppLaunch, setFirstAppLaunch] = useState("");
+  const [finishedOnboarding, setFinishedOnboarding] = useState(false);
+  const { isLogin } = useSelector(state => state.users);
+
+  useEffect(() => {
+    const getAsyncProps = async () => {
+      const appData = await AsyncStorage.getItem("firstAppLaunch");
+      if (appData === null) {
+        setFirstAppLaunch(true);
+        AsyncStorage.setItem("firstAppLaunch", "false");
+      } else {
+        setFirstAppLaunch(false);
+      }
+    };
+    getAsyncProps();
+  }, [AsyncStorage]);
+
+  if (firstAppLaunch && isLogin && finishedOnboarding === false)
+    return <OnboardingScreen setFinishedOnboarding={setFinishedOnboarding} />;
 
   return (
     <ScrollView>
