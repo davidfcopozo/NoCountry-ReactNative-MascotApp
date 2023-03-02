b22d99cb57a7c429f274a979a8ebdedb34898e66
David Francisco <davidfco.pozo@gmail.com>
2023-03-02 10:09:13 -0400

    Codigo refactorizado
    Signed-off-by: David Francisco <davidfco.pozo@gmail.com>

--- Client/components/OnboardingSlide.js
+++ Client/components/OnboardingSlide.js
@@ -2,25 +2,25 @@ import { useTheme } from "@react-navigation/native";
 import { useWindowDimensions } from "react-native";
 import { View, Image, Text } from "react-native";
 
-const OnboardingSlide = ({ item }) => {
-  const { height, width } = useWindowDimensions();
+const OnboardingSlide = ({ image, title, description }) => {
+  const { width } = useWindowDimensions();
   const { colors } = useTheme();
   return (
     <View className="align-center mb-4" style={{ width }}>
-      <Image source={item?.image} style={{ width, resizeMode: "contain" }} className="h-[75%]" />
+      <Image source={image} style={{ width, resizeMode: "contain" }} className="h-[75%]" />
 
       <View className="self-center">
         <Text
           style={{ color: colors.text }}
           className="text-base font-bold mt-5 text-center max-w-[75%]"
         >
-          {item?.title}
+          {title}
         </Text>
         <Text
           style={{ color: colors.text }}
           className="text-xs mt-2 text-center max-w-[70%] leading-6"
         >
-          {item?.description}
+          {description}
         </Text>
       </View>
     </View>
